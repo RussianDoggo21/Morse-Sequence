3de54b72155d564af0be8c085c0799bf405eeef7
Thomas Pernot <thomas.pernot@edu.esiee.fr>
2025-05-14 16:43:56 +0200

    Refactoring croissante.py and decroissante.py

--- croissante.py
+++ croissante.py
@@ -1,107 +1,104 @@
 from simplextree import SimplexTree
 
-#Reproduis l'union entre un élément elmt et une liste l
-def union(elmt, l):
-    if elmt not in l:
-        l.append(elmt)
-        
-
-#Retourne la dimension d'un simplexe
-def dim(simplexe):
-    return len(simplexe) - 1
+# Return the dimension of a simplex
+def dim(simplex):
+    return len(simplex) - 1
 
 
-#Retourne le bord d'un simplexe
-def bord(simplexe):
-    n = dim(simplexe)
-    st = SimplexTree([simplexe])
-    bord = [sigma for sigma in st.simplices(n-1)]
-    return bord
+# Return the boundary of a simplex
+def boundary(simplex):
+    n = dim(simplex)
+    st = SimplexTree([simplex])
+    boundary = [sigma for sigma in st.simplices(n-1)]
+    return boundary
 
 
-#Retourne le cobord d'un simplexe
-def cobord(simplexe, K_init):
-    cobord = K_init.cofaces(simplexe)
-    i = 0  # Index pour parcourir la liste cobord
-    while i < len(cobord):  # Equivalent à "do...while" avec une seule condition
-        elmt = cobord[i]
+# Return the coboundary of a simplex
+def coboundary(simplexe, K_init):
+    coboundary = K_init.cofaces(simplexe)
+    i = 0  # Index to browse the list coboundary 
+    while i < len(coboundary):  # Equivalent to "do...while" with an only condition
+        elmt = coboundary[i]
         if dim(elmt) != dim(simplexe) + 1:
-            cobord.remove(elmt)
+            coboundary.remove(elmt)
         else:
-            i += 1  # Passer à l'élément suivant uniquement si aucun élément n'est supprimé
-    return cobord
+            i += 1  # Go to the next element only if no element has been removed
+    return coboundary
 
 
-#Retourne le simplexe v tel que :
-#   - v est dans bord
-#   - v n'est pas dans S
-def find_out(bord, S): 
+# Return the simplex v such that :
+#   - v is in s_list
+#   - v is not in S
+def find_out(s_list, S): 
     possibilities = []
-    #print(f"bord = {bord}")
-    #print(f"S = {S}")
-    for v in bord:
-        #print(f"elmt de bord : {v}")
+    for v in s_list:
         if v not in S:
             possibilities.append(v)
     return possibilities[0]
 
-#Trie la liste L de simplexes par dimension décroissante
+# Sort the simplices list L by decreasing dimension 
 def tri_dim(L):
     L.sort(key=dim, reverse=True)
     
-# =============================================================================================================================================================================== #
-
-def morse_seq_croissante(K_init):
+# Main function
+def morse_seq_increasing(K_init):
     
-    # Trier les simplexes de K_init par dimensions croissante
+    # Sort the simplices of K_init by increasing dimension 
     K = K_init.simplices()
     n = len(K)
     
-    # Initialisation des variables
-    i = 0
-    S = list()
-    W = list()
-    L = list()
-    ro = {s: 0 for s in K}
-    n_crit = 0
+    # Initialization of the variables
+    i = 0 # Index allowing to browse the simplices of K
+    W = list() # Morse Sequence to be returned
+    S = list() # Contains the already "used" simplices in the construction of W
+    L = list() # Contains the simplices of dimension p that can form a free pair (p-1, p) for W
+    rho = {s: 0 for s in K} # Contains the number of faces already "used" for W of each simplex s in K
+    n_crit = 0 # Counter of critical simplices in W
     
-    while i < n-1:  # i va de 0 à n-1
+    while i < n-1:  # i goes from 0 to n-1
+        
+        # Here, sigma is a critical simplex
         sigma = K[i]
         S.append(sigma)
         W.append(sigma) 
         n_crit += 1
         
-        for tau in cobord(sigma, K_init): 
-            ro[tau] += 1
+        # Update of rho and L
+        for tau in coboundary(sigma, K_init): 
+            rho[tau] += 1
             L.append(tau)
-            tri_dim(L)
+            tri_dim(L) # Sort L by decreasing dimension
         
+        # While we can add free pairs to W
         while len(L) > 0:
-            tau = L.pop()
-            
-            if ro[tau] == dim(tau):
-                v = find_out(bord(tau), S) 
-                W.append((v, tau))
-                union(v, S)
-                union(tau, S)
+            tau = L.pop() # Possible first element of the free pair, dimension p
+            if rho[tau] == dim(tau): # Check if tau can be used for a free pair of W
+                v = find_out(boundary(tau), S) # Second element of the free pair, dimension p-1
                 
-                c1 = cobord(v, K_init)
-                c2 = cobord(tau, K_init)
-                cobords = []
+                # Update of W and S
+                W.append((v, tau))
+                S.append(v)
+                S.append(tau)
+                
+                # Update of rho and L
+                c1 = coboundary(v, K_init) # all simplices of dimension p
+                c2 = coboundary(tau, K_init ) # all simplices of dimension p+1
+                coboundarys = []
                 for elmt in c1 + c2:
-                    union(elmt, cobords)
-                for mu in cobords: 
-                    ro[mu] += 1
-                    union(mu, L)
-                tri_dim(L)
-                    
+                    coboundarys.append(elmt) # No risk of duplicate due to the difference in dimension
+                for mu in coboundarys: 
+                    rho[mu] += 1
+                    L.append(mu)
+                tri_dim(L) # Sort L by decreasing dimension
+        
+        # If the current simplex has already been "used" for W and we didn't
         while K[i] in S and i < n-1: 
             i += 1
             
     return W, n_crit
     
 # =============================================================================================================================================================================== #
-"""
+import numpy as np
 #K_init = SimplexTree([[1,2,3]])
 def MakeFacesVectorized1(Nr,Nc):
 
@@ -133,4 +130,3 @@ seq, n_crit = morse_seq_croissante(K_init)
 
 print(seq, "\n")
 print(n_crit)
-"""
